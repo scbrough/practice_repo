c740e5a91a037a80d56fc4e391597b0b1ca445b6
Scott Brough <ballstarsmb@gmail.com>
2022-03-03 15:31:58 -0600

    Add initial practice file

---
+++ practice.rb
@@ -0,0 +1,11 @@
+print "What is first variable"
+a = gets.chomp 
+print "What is second variable"
+b = gets.chomp
+if a < b 
+  puts "First variable is less than second"
+elsif b < a 
+  puts "Second variable is less than first"     
+else
+  puts "Variables are equal."
+end
